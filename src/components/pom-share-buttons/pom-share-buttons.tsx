60649f873732d0772adb20cba7911b0dd8a5c1af
Matteo Bortolazzo <bortolazzo.matteo@gmail.com>
2018-11-17 14:57:44 +0100

    Share links on popup"

--- src/components/pom-share-buttons/pom-share-buttons.tsx
+++ src/components/pom-share-buttons/pom-share-buttons.tsx
@@ -11,6 +11,13 @@ export class ShareButtons {
 
   @Prop() heading: string;
 
+  private onLinkClicked(e, url: string) {
+    if (!url.startsWith('mailto:')) {
+      e.preventDefault();
+      window.open(url, 'sharer', 'toolbar=0,status=0,width=648,height=395');
+    }
+  }
+
   render() {
     return(
       <div class="share-buttons">
@@ -19,7 +26,7 @@ export class ShareButtons {
           <div class="popup-inner">
             {generateSocialButtons(this.heading, this.pageUrl).map(b =>
               <div class="social-button" >
-                <a href={b.url} target={b.icon === 'mail' ? '' : '_blank'}>
+                <a onClick={e => this.onLinkClicked(e, b.url)} href={b.url} target={b.icon === 'mail' ? '' : '_blank'}>
                   <ion-icon name={b.icon}></ion-icon>
                   <span class="description">{b.description}</span>
                 </a>
