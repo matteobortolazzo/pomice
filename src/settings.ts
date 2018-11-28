6381b763ba12be2c55734f4e57cc132f44555a46
Matteo Bortolazzo <bortolazzo.matteo@gmail.com>
2018-11-28 23:16:18 +0100

    Settings fix

--- src/settings.ts
+++ src/settings.ts
@@ -7,7 +7,5 @@ export const PROFILE_URL_TWITTER = 'https://twitter.com/matteobortolaz';
 export const PROFILE_URL_GITHUB = 'https://github.com/matteobortolazzo';
 export const PROFILE_URL_LINKEDIN = 'https://www.linkedin.com/matteobortolazzo';
 
-// export const CONTENTFUL_SPACE_ID = null;
-// export const CONTENTFUL_ACCESS_TOKEN = null;
-export const CONTENTFUL_SPACE_ID = '51lkc6dcu1f8';
-export const CONTENTFUL_ACCESS_TOKEN = '11d8873bdeabd732170e9464b3e50e656711f003c6ac3cab3e6a14a9be741f44';
+export const CONTENTFUL_SPACE_ID = null;
+export const CONTENTFUL_ACCESS_TOKEN = null;
